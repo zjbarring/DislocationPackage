ccf1f3b00aa4708455c823bce4c7041563523e0c
zjbarring <105514138+zjbarring@users.noreply.github.com>
2022-05-13 15:54:51 -0400

    Add files via upload

--- DislocationDisplacement.m
+++ DislocationDisplacement.m
@@ -1,8 +1,9 @@
-function [u] = DislocationDisplacement(theta, b1, b2, b3, nu, phs1, phs2, phs3, vshift)
+function [u] = DislocationDisplacement(theta, b1, b2, b3, nu, phs1, phs2, phs3)
 %reads in some polar space, 3 coefficients reperesenting burgers vector
 %projections, and 3 phase shifts to align the data.
 
-u =  b3/2/pi* (theta-phs3)+ b3/2/pi*(sin(2*(theta-phs3))/4/(1-nu)) + ...
-    b2/2/pi*(cos(2*(theta-phs2))/4/(1-nu)) + b1/2/pi*(theta-phs1) + vshift;
+u = b3/2/pi* ((theta-phs3)+(sin(2*(theta-phs3))/4/(1-nu))) + ...
+    b2/2/pi*((cos(2*(theta-phs2))/4/(1-nu))) + b1/2/pi*(theta-phs1);
 
 end
+
