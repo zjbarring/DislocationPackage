842edef6db1a9a44ad4e492053c57424a927db2f
zjbarring <105514138+zjbarring@users.noreply.github.com>
2022-11-08 16:09:46 -0500

    Add files via upload

--- Dislocation_Tracing_Ulvestad.m
+++ Dislocation_Tracing_Ulvestad.m
@@ -1,25 +1,38 @@
-close all;
+clear; close all;
 
-threshold = 0.1;
+threshold = 0.000001;
+filename = 'CBNO/June22 Beamtime Measurements/L0_after.mat';
+exp_filename = 'CBNO/June22 Beamtime Measurements/L0_after_coretrace.tiff';
 
-[val] = bruteforce3('complexRho_GaAs.mat', threshold);
-load('complexRho_GaAs.mat');
+% %temporary
+% load('Peer Edits and revisions/d33 paper/d33_displacement_E123.mat');
+% mask = uint8(uu1_aa~=0);
 
-disl_flags = val>4;
-gridsize = size(val);
-[xx,yy,zz] = ndgrid(1:1:gridsize(1), 1:1:gridsize(2), 1:1:gridsize(3));
+[val] = bruteforce3(filename, threshold);
 
-disl_points = [val(disl_flags), xx(disl_flags), yy(disl_flags), zz(disl_flags)];
+%Load reconstruction into the variable 'complexCrystal'
+S = load(filename);
+varName = fieldnames(S);
+complexCrystal = S.(varName{1});
+
+%% Group Points & Save results
+% disl_flags = val>4;
+% gridsize = size(val);
+% [xx,yy,zz] = ndgrid(1:1:gridsize(1), 1:1:gridsize(2), 1:1:gridsize(3));
+% 
+% disl_points = [val(disl_flags), xx(disl_flags), yy(disl_flags), zz(disl_flags)];
+
+%ind=clusterdata(disl_points, 3);
 
 
 
 options.overwrite = 'true';
 
-saveastiff(ind, 'disl_cluster.tiff', options);
+saveastiff(val, exp_filename , options);
 
 
 
-%Visualization:
+%% Visualization:
 
 %Whole network of dislocations in one color
 figure; p = patch(isosurface(val, 4), 'FaceColor', 'yellow', 'EdgeColor', 'blue');
@@ -29,22 +42,22 @@ camlight;
 
 axis equal; axis off;
 
-p = patch(isosurface(abs(complexRho_GaAs),.1));%alpha(0.4);
+p = patch(isosurface(abs(complexCrystal),threshold));%alpha(0.4);
 set(p,'facecolor','red','edgecolor','none','facealpha',0.2);
 
-hold on
-sfs = Tiff2Var('true_gradz.tiff');
-patch(isosurface(sfs, 0.5), 'FaceColor', 'yellow', 'EdgeColor', 'black')
-hold off
+% hold on
+% sfs = Tiff2Var('true_gradz.tiff');
+% patch(isosurface(sfs, 0.5), 'FaceColor', 'yellow', 'EdgeColor', 'black')
+% hold off
 
 
 
 %Dislocation points grouped by proximity
 
-Dislocations = IsolateDislocation(val, 7);
+% Dislocations = IsolateDislocation(val, 7);
 
-figure;
-hold on;
+% figure;
+% hold on;
 
 % p1 = patch(isosurface(Dislocations(1).dislocation, 4), 'FaceColor', 'yellow', 'EdgeColor', 'yellow');
 % p2 = patch(isosurface(Dislocations(2).dislocation, 4), 'FaceColor', 'red', 'EdgeColor', 'red');
